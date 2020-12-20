cd9df31e11fdc2faec9279cae684f97946c15e00
ellilachen <ellilachen@tencent.com>
2020-12-20 17:33:58 +0800

    feat: 无重叠区间

--- greed/basic.py
+++ greed/basic.py
@@ -1,23 +1,73 @@
 # coding=utf-8
 # (Easy)455. 分发饼干
+# class Solution(object):
+#     def findContentChildren(self, g, s):
+#         """
+#         :type g: List[int]
+#         :type s: List[int]
+#         :rtype: int
+#         """
+#         g.sort()
+#         s.sort()
+#         child = 0
+#         for cookie in s:
+#             if child == len(g):
+#                 break
+#             if cookie >= g[child]:
+#                 child += 1
+
+#         return child
+
+
+# solution = Solution()
+# print(solution.findContentChildren([1, 2, 3], [1, 1]))
+
+
+# (Medium) 435. 无重叠区间
 class Solution(object):
-    def findContentChildren(self, g, s):
+    def takeSecond(self, elem):
+        return elem[1]
+
+    def eraseOverlapIntervals(self, intervals):
         """
-        :type g: List[int]
-        :type s: List[int]
+        :type intervals: List[List[int]]
         :rtype: int
         """
-        g.sort()
-        s.sort()
-        child = 0
-        for cookie in s:
-            if child == len(g):
-                break
-            if cookie >= g[child]:
-                child += 1
-
-        return child
+        # 按区间尾排序
+        if len(intervals) == 0:
+            return 0
+        intervals.sort(key=self.takeSecond)
+        prev = intervals[0][1]
+        count = 0
+        for idx in range(1, len(intervals)):
+            if intervals[idx][0] < prev:
+                count += 1
+            else:
+                prev = intervals[idx][1]
+        return count
 
 
 solution = Solution()
-print(solution.findContentChildren([1, 2, 3], [1, 1]))
+print(solution.eraseOverlapIntervals([]))
+
+# (Easy) 453. 最小操作次数使数组元素相等
+# 暴力法 每次增加1，循环判断最小 != 最大时，排序，除了最后一位，都+1
+# 暴力法改进2  循环判断最小 != 最大时，排序，除了最后一位，都增加（最大-最小）
+# 暴力法改进3  其实并不需要真正改变数组
+# class Solution(object):
+#     def minMoves(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         nums.sort()
+#         count = 0
+#         index = len(nums) - 1
+#         while index > 0:
+#             count += nums[index] - nums[0]
+#             index -= 1
+#         return count
+
+
+# solution = Solution()
+# print(solution.minMoves([5, 6, 8, 8, 5]))
--- greed/introduction.md
+++ greed/introduction.md
@@ -1,7 +1,16 @@
 1. [贪心算法：](https://time.geekbang.org/column/article/73188)
-   1. [霍夫曼编码（Huffman Coding）](https://zhuanlan.zhihu.com/p/134120024)
-   2. Prim 和 Kruskal 最小生成树算法
-   3. Dijkstra 单源最短路径算法。
 2. 分治算法
 3. 回溯算法
 4. 动态规划
+
+### 贪心算法
+
+1. [霍夫曼编码（Huffman Coding）](https://zhuanlan.zhihu.com/p/134120024)
+2. Prim 和 Kruskal 最小生成树算法
+3. Dijkstra 单源最短路径算法。
+
+LeetCode 题型
+
+1. 分配问题
+2. 区间问题
+   注意 在处理数组前，统计一遍信息(如频率、个数、第一次出现位置、最后一次出现位置等)可 以使题目难度大幅降低。
